6e78a3e7b9db62f1af5c5913d720439beec2fe52
Brad White <bradscottwhite@gmail.com>
2023-08-23 01:45:13 -0400

    ...

--- main.go
+++ main.go
@@ -1,27 +1,30 @@
 package main
 
 import (
-	"log"
 	"os"
+
 	"github.com/gofiber/fiber/v2"
 )
 
-func main() {
-    app := fiber.New()
-
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
-
-	app.Get("/env", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, ENV! " + os.Getenv("TEST_ENV"))
-	})
-
+func getPort() string {
 	port := os.Getenv("PORT")
-
 	if port == "" {
-		port = "3000"
+		port = ":3000"
+	} else {
+		port = ":" + port
 	}
 
-	log.Fatal(app.Listen("0.0.0.0:" + port))
+	return port
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello, Railway!",
+		})
+	})
+
+	app.Listen(getPort())
 }
